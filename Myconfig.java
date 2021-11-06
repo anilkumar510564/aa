01cf28bbe03e0b33ffa6d5e76afb2fd5494b5fbf
anilkumar510564 <68316602+anilkumar510564@users.noreply.github.com>
2021-11-06 11:11:26 +0530

    Update Myconfig.java

--- Myconfig.java
+++ Myconfig.java
@@ -8,3 +8,56 @@ import org.springframework.context.annotation.Configuration;
 public class MyConfig {
 
 }
+
+
+
+package com.trainticket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class MyController {
+    @RequestMapping("/login")
+    public ModelAndView login(HttpServletRequest request, HttpServletResponse response) {
+        String userName = request.getParameter("username");
+        String password = request.getParameter("password");
+        ModelAndView mv = new ModelAndView();
+        if (userName.equals("train") && password.equals("train")) {
+            mv.setViewName("index.jsp");
+        } else {
+            mv.setViewName("failure.jsp");
+        }
+        return mv;
+    }
+}
+
+
+
+
+
+package com.trainticket;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebInit extends  AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {MyConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/login"};
+    }
+}
